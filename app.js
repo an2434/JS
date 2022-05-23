7f6d17fd002163fb980c99d28de4459b0a5a7662
an2434 <jinhyung2434@naver.com>
2022-05-23 21:00:04 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-function sayHello(nameOfPerson,age){
-    console.log( "qwe "+nameOfPerson+ " ewr"+ age );
+function plus(a,b){
+    console.log(a+b);
+
 }
-sayHello("nico", 21);
-sayHello("nifao", 23);
-sayHello("nsdo", null);
+
+plus();

04f9aeea23d39a52cf78b55f335069fd8a1b0ac4
an2434 <jinhyung2434@naver.com>
2022-05-30 22:50:26 +0900

    Update app.js

--- app.js
+++ app.js
@@ -17,9 +17,36 @@ const me = "sexy"
 const days = [1,2,false, true,null,undefined,me,"text"];
 console.log(days);*/
 
+/*
 const toBuy = ["감자","토마토","피자"];
 console.log(toBuy);
 toBuy[2] = "water";
 console.log(toBuy);
 toBuy.push("one");
 console.log(toBuy);
+*/
+/*
+const player = { 
+    name : "nico", 
+    age: 98,
+};
+console.log(player);
+player.name = " niqwe";
+console.log(player);
+player.qwe = "soon";
+console.log(player,console);
+*/
+/*
+function plus(){
+console.log(2+2);
+
+}
+alert("asasd");
+*/
+function plus(po,sa){
+    console.log(po+sa);
+
+}
+plus(12,23);
+plus(1.2,2312);
+plus(12,2.312);

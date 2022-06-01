e03e30ae83fb95e8e65acadbdef41c14a79fc485
an2434 <jinhyung2434@naver.com>
2022-06-02 08:01:05 +0900

    js basic grammar

--- app.js
+++ app.js
@@ -95,5 +95,49 @@ const a ={
    const qw2 = a.qwe2(2,3);
    const qw3 = a.qwe3(10,9);*/
    //const name = prompt("너 누구야");
-const age =prompt("몇살");
-   console.log( age,parseInt(age));
+/*const age =parseInt(prompt("몇살"));
+
+if(isNaN (age))
+{
+    age === true
+ console.log("숫자를 입력해주세요");
+}
+*/
+/*
+const age =parseInt(prompt("몇살"));
+
+if(isNaN(age))
+{
+ console.log("숫자를 입력해주세요");
+}
+else{
+    console.log("나이 입력됨");
+}
+*/
+/*
+const age = parseInt(prompt("나이를 입력해주세요"));
+
+if(isNaN(age)){
+    console.log("숫자를 입력해주세요");
+
+}
+else if(age<18)
+{
+    console.log("당신은 어려요");
+}
+else if(age <= 50 && age >= 18)
+{
+    console.log("당신은 술을 마실 수 있는 나이입니다");
+}
+else if(age > 50 && age <= 80){
+    console.log("운동해야됨");
+}
+else if (age>80);
+{
+    console.log(
+         
+    )
+}*/
+
+const title = document.getElementById("title")
+title.innerText = "got you";

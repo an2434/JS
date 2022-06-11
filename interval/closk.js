6d8b0a1fd8137092266c7da2071b02947834ac9a
안진형 <81676396+an2434@users.noreply.github.com>
2022-06-11 22:16:58 +0900

    js basic grmmar
    clock2

--- interval/closk.js
+++ interval/closk.js
@@ -2,7 +2,12 @@ const clock = document.querySelector("h2#clock");
 
 function getclock() {
     const date = new Date();
-   clock.innerText =`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+   
+    const hours = String(date.getHours()).padStart(2,"0");
+    const mins =String(date.getMinutes()).padStart(2,"0");
+    const sec = String(date.getSeconds()).padStart(2,"0");
+  // clock.innerText =`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+   clock.innerText =`${hours}:${mins}:${sec}`;
 
 }
 getclock();

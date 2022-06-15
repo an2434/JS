0d404e725b1305b0b2474508360c662fdbac36f7
an2434 <jinhyung2434@naver.com>
2022-06-15 14:33:14 +0900

    Update todo.js

--- interval/todo.js
+++ interval/todo.js
@@ -1,6 +1,16 @@
 const todoform = document.getElementById("todo-form");
 const todoinput = document.querySelector("#todo-form input")
 const todolist = document.getElementById("todo-list");
+
+const todos = [];
+
+const TODOS_KEY ="todos";
+
+function savetodos(){
+    localStorage.setItem(TODOS_KEY,JSON.stringify(todos));
+   
+}
+
 function deletetodo(){
     const li = event.target.parentElement;
     li.remove();
@@ -25,10 +35,22 @@ function todosubmit(event){
   
     const newtodo = todoinput.value;
     todoinput.value ="";
-   
+    todos.push(newtodo);
     painttodo(newtodo);
+    savetodos();
     event.preventDefault();
    // console.log(todoinput.value);
 }
 
 todoform.addEventListener("submit",todosubmit);
+
+
+
+const savetodos = localStorage.getItem(TODOS_KEY);
+console.log(savetodos);
+if(savetodos !== null){
+    const parsedtodos = JSON.parse(savetodos);
+    parsedtodos.forEach(element => {
+        
+    });
+}
